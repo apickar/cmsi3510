3655ff0bbafd4dc7d1174ab828cf6156b76f8171
apickar <70719073+apickar@users.noreply.github.com>
2023-10-26 08:37:06 -0700

    Update dining philsophers

--- chopstick.go
+++ chopstick.go
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream:chopstick.go
 package main
 
 import "sync"
@@ -7,3 +8,14 @@ type ChopStick struct {
 
 	ID int
 }
+=======
+package main
+
+import "sync"
+
+type ChopStick struct {
+	sync.Mutex
+
+	ID int
+}
+>>>>>>> Stashed changes:dining-philosopher/chopstick.go
